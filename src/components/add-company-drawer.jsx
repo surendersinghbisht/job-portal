357cefaf202e52c75b71bd483a519d12aba592ad
surender singh <singhsurender28231@gmail.com>
2025-02-17 00:06:15 +0530

    fixed bug

--- src/components/add-company-drawer.jsx
+++ src/components/add-company-drawer.jsx
@@ -13,7 +13,7 @@ import {
   import { z } from "zod";
   import { zodResolver } from "@hookform/resolvers/zod";
   import { useForm } from "react-hook-form";
-  import useFetch from "@/hooks/use-fetch";
+  import useFetch from "@/HOOKS/use-fetch";
   import { addNewCompany } from "@/api/apicompanies";
   import { BarLoader } from "react-spinners";
   import { useEffect } from "react";
